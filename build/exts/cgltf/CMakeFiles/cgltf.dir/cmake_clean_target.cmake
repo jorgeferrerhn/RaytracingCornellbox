b1ad28eb7366861ed47879e1f36a208d5fca2390
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2023-01-27 22:55:29 +0100

    Delete build/

--- build/exts/cgltf/CMakeFiles/cgltf.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/jorge/Escritorio/RaytracingCornellbox/bin/libcgltf.a"
-)
--- build/exts/stb_image/CMakeFiles/stb_image.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/jorge/Escritorio/RaytracingCornellbox/bin/libstb_image.a"
-)
